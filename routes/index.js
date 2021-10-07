9ccf585adef193e6720d809a6e88ba55d547dccc
blake <blake.pinette@gmail.com>
2021-10-07 08:04:02 -0700

    add some files I forgot to include

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const path = require('path');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/entry', function(req, res){
+    res.render('entry');
+});
+router.get('/search', function(req, res){
+    res.render('search');
 });
 
 module.exports = router;
